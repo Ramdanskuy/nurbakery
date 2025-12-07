59b64f07bfb16c5e9cbc523b24f81b01f5fd80a0
Ramdanskuy <145859890+Ramdanskuy@users.noreply.github.com>
2025-12-07 09:23:43 +0700

    Enhance navbar and add search/cart toggle functionality

--- js/script.js
+++ js/script.js
@@ -1,16 +1,43 @@
 // TOGGLE CLASSS ACTIVE
 const navbarNav = document.querySelector('.navbar-nav')
-
-// WHEN MENU-BTN DI KLIK
+// ketika hamburger menu di klik
 document.querySelector('#menu-btn').onclick = () => {
-    navbarNav.classList.toggle('active');
+  navbarNav.classList.toggle('active');
 };
 
-// KLIK DI LUAR SIDEBAR UNTUK MENGHILANGKAN NAV
-const menu = document.querySelector('#menu-btn')
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-document.addEventListener('click', function(e) {
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
+document.querySelector('#search-btn').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-btn').onclick = (e) => {
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector('#menu-btn');
+const sb = document.querySelector('#search-btn');
+const sc = document.querySelector('#shopping-cart-btn');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
+
